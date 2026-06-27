357d78d34ab55e7d8d410da88437e6a46838b6ef
JonathanOrozcoCervantes <60130379+JonathanOrozcoCervantes@users.noreply.github.com>
2026-06-26 19:45:30 -0600

    minor

--- server/config/settingsLocal.example.js
+++ server/config/settingsLocal.example.js
@@ -1,6 +1,6 @@
 module.exports = {
   FIREBASE_PROJECT_ID: 'chat-action-gateway',
   FUNCTION_REGION: 'us-central1',
-  APP_CHECK_ENFORCEMENT: false, // Avoid app check use false
+  APP_CHECK_ENFORCEMENT: true, // Avoid app check use false
   OAUTH_ACCESS_TOKEN_SECRET: '' // Manual random token
 };
